3bfff1f6eddd26c44fdbbcb9096c2c85266031e3
Paweł Kiszteliński <pkisztelinski@gmail.com>
2018-12-07 16:10:53 +0100

    day 5 - part2

---
+++ day-5/part_1_and_2.py
@@ -0,0 +1,28 @@
+input_data = [line.rstrip('\n') for line in open('input.txt')][0]
+
+
+def create_polymer_or_something(data):
+    text_tmp = []
+    for c in data:
+        if text_tmp and ((c.upper() == text_tmp[-1].upper()) and (c != text_tmp[-1])):
+            text_tmp.pop()
+        else:
+            text_tmp.append(c)
+    return text_tmp
+
+
+def calculate_part2(data):
+    parsed_chars = set()
+    results = set()
+    for c in data:
+        if c.upper() not in parsed_chars:
+            tmp_text = data.replace(c.upper(), "")
+            tmp_text = tmp_text.replace(c.lower(), "")
+            results.add(len(create_polymer_or_something(tmp_text)))
+            parsed_chars.add(c.upper())
+
+    return results
+
+
+print("Part 1: ", len(create_polymer_or_something(input_data)))
+print("Part 2: ", min(calculate_part2(input_data)))
