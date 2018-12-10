d342cd0ff879002ca56ad8469d997a2c8fa53546
Paweł Kiszteliński <pkisztelinski@gmail.com>
2018-12-10 20:26:13 +0100

    day 6 part 2

--- day-6/part_1_and_2.py
+++ day-6/part_1_and_2.py
@@ -1,9 +1,6 @@
-from time import time
 import itertools
 from collections import defaultdict
-
-t0 = time()
-
+from functools import reduce
 
 with open("input.txt") as file:
     input_data = set([
@@ -38,18 +35,29 @@ def check_if_infinite(point, x1x1, x2x2):
     return False
 
 
-def part_1(data):
+def get_xy_points(data):
     x1y1 = (min([p[0] for p in data]) - 1, min([p[1] for p in data]) - 1)
     x2y2 = (max([p[0] for p in data]) + 1, max([p[1] for p in data]) + 1)
 
+    return x1y1, x2y2
+
+
+def get_xy_lines(x1y1, x2y2):
     x_line = tuple([x for x in range(x1y1[0], x2y2[0] + 1)])
     y_line = tuple([x for x in range(x1y1[1], x2y2[1] + 1)])
+
+    return x_line, y_line
+
+
+def part_1(data):
+    x1y1, x2y2 = get_xy_points(data)
+    x_line, y_line = get_xy_lines(x1y1, x2y2)
 
     best_points = defaultdict(set)
     infinite_goals = set()
 
     for point in itertools.product(x_line, y_line):
-        goals, distance = calculate_shortest_distance(point, input_data)
+        goals, distance = calculate_shortest_distance(point, data)
 
         if len(goals) > 1:
             continue
@@ -57,16 +65,25 @@ def part_1(data):
         for goal in goals:
             best_points[goal].add(point)
 
-        if check_if_infinite(point, x1y1, x2y2):
-            infinite_goals.add(next(iter(goals)))
-
     finite_goals = data - infinite_goals
 
     return max([len(best_points[g]) for g in finite_goals])
 
 
+def part_2(data):
+    x1y1, x2y2 = get_xy_points(data)
+    x_line, y_line = get_xy_lines(x1y1, x2y2)
+
+    result = 0
+    for point in itertools.product(x_line, y_line):
+        distance = reduce(
+            lambda x, y: x + y, [calculate_distance(point, goal) for goal in data])
+
+        if distance < 10000:
+            result += 1
+
+    return result
+
+
 print(part_1(input_data))
-
-t1 = time()
-
-print("%f" % (t1 - t0))
+print(part_2(input_data))
